5d998fe9d8265387426b469935b3f7626e4864ff
dbcoder0625 <107257463+dbcoder0625@users.noreply.github.com>
2023-12-04 07:43:22 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,1 +1,16 @@
-
+try:
+  nu=0
+  while True:
+    s=input()
+    start="";end="" 
+    for i in range(len(s)):
+      if s[i].isnumeric():
+        start=s[i] 
+        break 
+    for i in range(len(s)-1,-1,-1):
+      if s[i].isnumeric():
+        end=s[i] 
+        break 
+    nu+=int(start+end) 
+except:
+  print(nu)
